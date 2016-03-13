68b06a3b92d1057c6bc74a2032a0887444e4b482
bogotobogo <k@bogotobogo.com>
2016-03-12 20:50:29 -0800

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="main-container">
+         <h1>Xophist</h1>
+         <p class="lead">Powered by Laravel</p>
+
+         <hr>
+
+         <div class="row">
+            <div class="col-sm-6">
+                <h2>Topic 1</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. </p>
+                <p><a href="#" role="button" class="btn btn-default">View more »</a></p>
+            </div>
+            <div class="col-sm-6">
+                <h2>Topic 2</h2>
+                <p>Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. 
+</p>
+                <p><a href="#" role="button" class="btn btn-default">View more »</a></p>
+            </div>
+        </div>
+    </div>
+@stop
